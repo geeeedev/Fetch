1d936e01bbfbf3c4f9ebf198672c37f20ef9d9e0
Gwennie Lau <geeeedev@gmail.com>
2021-08-10 12:07:26 -0700

    debugging

--- api/managed-records.js
+++ api/managed-records.js
@@ -7,6 +7,7 @@ window.path = "http://localhost:3000/records";
 // Your retrieve function plus any additional functions go here ...
 const generateResultObj = (data, pg) => {
 
+  console.log(`Transforming Data ...`);
   //ids: [id, id, id],
   const idList = data.map((item) => item.id);
 
@@ -32,66 +33,69 @@ const generateResultObj = (data, pg) => {
   let firstPg = 1;
   const prevPg = pg > firstPg ? pg - 1 : null;
 
-  console.log(`data.length`, data.length, `Math.ceil always 1`, Math.ceil(data.length / 10));
   //nextPage: # || null
   let lastPg = 50;
   const nxtPg = pg < lastPg ? pg + 1 : null;
 
+  //return JSON.stringify({
   return {
     previousPage: prevPg,
     nextPage: nxtPg,
     ids: idList,
     open: openList,
-    closedPrimaryCount: closedPrimCount
+    closedPrimaryCount: closedPrimCount,
   };
 };
 
 //retrieve( { page:2, colors: ["red","brown"] } );   => { limit: x, offset: x, colors: [x,x,x] }
-const retrieve = (optionsObj = {}) => {
+const retrieve = (optionsObj = { page: 0, colors: [] }) => {
   // get page and colors
-  const page = optionsObj.page || 1;        //2
-  const colorArr = optionsObj.colors || [   //["red","brown"]
+  const page = optionsObj.page || 1; //2
+  const colorArr = optionsObj.colors || [
+    //["red","brown"]
     "red",
     "brown",
     "blue",
     "yellow",
     "green",
-  ]; 
+  ];
+  console.log(`page`, page, `colorArr`, colorArr);
 
   //convert page and color array into query string options: limit, offset, and color[]
-  const itemPerPage = 10;                   // process pages of 10 items at a time
-  const derivedStartIdx = (page - 1) * 10;  // pg 1 => start @ idx 0; pg 2 => start @ idx 10; pg 3 => start @ idx 20 ...
+  const itemPerPage = 10; // process pages of 10 items at a time
+  const derivedStartIdx = (page - 1) * 10; // pg 1 => start @ idx 0; pg 2 => start @ idx 10; pg 3 => start @ idx 20 ...
 
-  // create URI as 'http://localhost:3000/records?limit=2&offset=0&color=brown&color=green'
-  let uriEndpoint = new URI(window.path).search({
+  // create URI as 'http://localhost:3000/records?limit=2&offset=0&color=red&color=brown&color=blue&color=yellow&color=green'
+  let uriEndpoint = new URI(window.path).setSearch({
     limit: itemPerPage,
     offset: derivedStartIdx,
-    color: colorArr,
+    color: colorArr, //'color[]': works too
   });
-  console.log(`uriEndpoint`,uriEndpoint.toString());
+  console.log(`uriEndpoint`, uriEndpoint.toString());
 
+
+  let resultData;
   fetch(uriEndpoint)
-//   fetch(uriEndpoint.toString())  //got more response - good?
+//fetch(uriEndpoint.toString())  //same
     .then((res) => {
-      console.log(`fetch-res`,res);                 //check status 200 OK
-    //console.log(`fetch-res.json`,res.json());     //check res.json() result
-      return res.json();                            //convert response object into a JS obj
+      console.log(`fetch-res`, res); //check status 200 OK
+      return res.json(); //convert response object into a JS obj
     })
     .then((data) => {
       console.log(`Endpoint Success-data:`, data);
-      //run the parse method here
-      console.log(`page`,page);
-      console.log(`Endpoint Success-2`, JSON.stringify(generateResultObj(data, page)));
-      return JSON.stringify(generateResultObj(data, page));
+    //console.log(`Endpoint Success-page`, page);
+    //transform data
+      resultData = generateResultObj(data, page);
+    console.log(`Endpoint Success-2`, resultData);
     })
     .catch((err) => {
       console.log(`Endpoint Error:`, err);
     });
+
+  return Promise.resolve(resultData);
 };
 
 export default retrieve;
 
 // LAST:
 // dont forget to refactor code
-
-// handle uriEndpoint color with []
